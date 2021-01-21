58db20693bd1d81a3bec900eefafad4148614630
TimeForANinja <TimeForANinja@users.noreply.github.com>
2021-01-21 10:05:23 +0100

    upload first test template

---
+++ test/test_prev.js
@@ -0,0 +1,20 @@
+const counts = new Array(10).fill(1e5).map(x => Math.floor(x * Math.random()));
+
+const mods = new Map();
+mods.set('custom bit-meddler', require('../custom_bit_meddler.js'));
+// mods.set('custom', require('../script.js'));
+
+for (const [name, mod] of mods) {
+  const results = [];
+  for (const c of counts) {
+    const m = new mod(c, 1337, 69);
+    const all = m.all().reverse();
+    const all_rev = [all[0]];
+    for(let i = all.length - 1 ; i > 0 ; i--) {
+      all_rev.push(m.prev());
+    }
+    // console.log(all, all_rev);
+    results.push(`${c} = ${all.some((item, index) => item !== all_rev[index]) ? 'failed' : 'success'}`);
+  }
+  console.log(`${name} achieved: ${results.join(', ')}`);
+}
